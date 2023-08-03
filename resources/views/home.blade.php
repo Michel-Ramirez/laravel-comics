cba613ab0e0eb2e16ab39afe95d69813b508d038
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-08-03 16:02:11 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    @extends('layouts.main_layout')
+
+    @section('title', 'Home')
+
+    @section('main-content')
+        <h1>main</h1>
+    @endsection
+</body>
+
+</html>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="header-top">
+        <div class="container">
+            <a href="#">DC POWER VISA</a>
+            <a href="#">ADDITIONAL DC SITES</a>
+        </div>
+    </div>
+</header>
